6e35b089421d0d2b9a39e66eece1ed566899ee07
2044smile <2044smile@naver.com>
2023-03-23 22:02:51 +0900

    :sparkles: feat: make human_like views and urls and html

--- prography/urls.py
+++ prography/urls.py
@@ -1,4 +1,7 @@
+from .views import HumanLikeListView
+
 from django.urls import path
 
 urlpatterns = [
+    path('human_like/', HumanLikeListView.as_view(), name='HumanLike'),
 ]
--- prography/views.py
+++ prography/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Human
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import ListView
+
+
+class HumanLikeListView(ListView):
+    model = Human
+    paginate_by = 5
+
+    def get_ordering(self):
+        ordering = self.request.GET.get('ordering', 'likes')
+        return ordering
