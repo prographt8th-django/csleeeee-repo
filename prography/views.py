5521e4f119bdadcbf411a67a6b88077fec302e3a
2044smile <2044smile@naver.com>
2023-03-24 15:56:52 +0900

    :poop: refactor: I use cache but I don't know correctly use or not

--- prography/views.py
+++ prography/views.py
@@ -1,7 +1,13 @@
 from .models import Human
 
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView
+from django.core.cache import cache
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from rest_framework.response import Response
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
 class HumanLikeListView(ListView):
@@ -11,3 +17,22 @@ class HumanLikeListView(ListView):
     def get_ordering(self):
         ordering = self.request.GET.get('ordering', 'likes')
         return ordering
+    
+
+class HumanLikeCreateUpdateView(CreateView, UpdateView):
+    model = Human
+
+    def get(self, request, *args: str, **kwargs):
+        if 'likes' in cache:
+            likes = cache.get('likes')
+            return Response(likes)
+        else:
+            human = Human.objects.all()
+            results = [h.to_json() for h in human]
+            cache.set(human, results, timeout=CACHE_TTL)
+
+    def post(self, request, *args: str, **kwargs):
+        if 'likes' in cache:
+            likes = cache.set('likes')
+            return Response(likes)
+
