3228eb6044b6d4fec266de0345997d75e8527a77
2044smile <2044smile@naver.com>
2023-04-05 07:04:59 +0900

    :sparkles: feat: set rest_framework

--- prography/urls.py
+++ prography/urls.py
@@ -1,8 +1,9 @@
-from .views import HumanLikeListView, HumanLikeCreateUpdateView
+from .views import HumanLikeListView, HumanLikeCreateUpdateView, HumanListAPIView
 
 from django.urls import path
 
 urlpatterns = [
+    path('human/', HumanListAPIView.as_view(), name='Human'),
     path('human_like/', HumanLikeListView.as_view(), name='HumanLike'),
     path('human_like/<int:pk>/',HumanLikeCreateUpdateView.as_view(), name='HumanLikeIt')
 ]
