184d5faea35c334ba91b001f122d066f4218f965
2044smile <2044smile@naver.com>
2023-04-06 21:02:46 +0900

    :sparkles: feat: installed swagger

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from rest_framework import routers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+router = routers.DefaultRouter()
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="PROGRAPHY API",
+        default_version="v0.0.1",
+        description="Welcome PROGRAPHY",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="2044smile@naver.com"), license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +40,10 @@ urlpatterns = [
     path('prography/', include('prography.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
