b29c773155b4b17b0dfd84d5dca79a64cc1daa16
2044smile <2044smile@naver.com>
2023-04-11 20:19:30 +0900

    :sparkles: feat: add HumanDetailAPIView

--- prography/urls.py
+++ prography/urls.py
@@ -1,9 +1,10 @@
-from .views import HumanLikeListView, HumanLikeDetailView, HumanListAPIView
+from .views import HumanLikeListView, HumanLikeDetailView, HumanListAPIView, HumanDetailAPIView
 
 from django.urls import path
 
 urlpatterns = [
     path('human/', HumanListAPIView.as_view(), name='Human'),
+    path('human/<int:pk>/', HumanDetailAPIView.as_view(), name='Human'),
 
     path('human_like/', HumanLikeListView.as_view(), name='HumanLike'),
     path('human_like/<int:pk>/',HumanLikeDetailView.as_view(), name='HumanLikeIt')
