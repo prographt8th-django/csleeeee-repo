6aced47b638c16144afa57f8801edd0a335e4dad
2044smile <2044smile@naver.com>
2023-04-18 20:57:12 +0900

    :recycle: refactor: change Human to HumanDetail

--- prography/urls.py
+++ prography/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('human/', HumanListAPIView.as_view(), name='Human'),
-    path('human/<int:pk>/', HumanDetailAPIView.as_view(), name='Human'),
+    path('human/<int:pk>/', HumanDetailAPIView.as_view(), name='HumanDetail'),
 
     path('human_like/', HumanLikeListView.as_view(), name='HumanLike'),
     path('human_like/<int:pk>/',HumanLikeDetailView.as_view(), name='HumanLikeIt')
