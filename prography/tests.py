00f7be2e1c63225e78f0dee045f1acdd20459628
2044smile <2044smile@naver.com>
2023-04-19 18:29:48 +0900

    :sparkles: feat: I prepared a TestCode

--- prography/tests.py
+++ prography/tests.py
@@ -6,6 +6,14 @@ from .models import Human
 
 
 class HumanTestCase(TestCase):
+    def setUp(self):
+        Human.objects.create(name="cslee", description="healthful guy", likes=0)
+        # Human.objects.create(name="AI", description="healthful AI", likes=99)
+
     def test_queryset_exists(self):
         qs = Human.objects.all()
         self.assertTrue(qs.exists)
+
+    def test_queryset_correct(self):
+        qs = Human.objects.get(name="cslee")
+        self.assertEqual(qs.name, "cslee")
