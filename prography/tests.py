4ec584ca66ebf240962513902a99ac8a9dfce2be
2044smile <2044smile@naver.com>
2023-04-24 23:27:39 +0900

    :recycle: refactor: modify models def save and working

--- prography/tests.py
+++ prography/tests.py
@@ -1,6 +1,4 @@
 from django.test import TestCase
-from django.core.serializers import serialize
-from unittest.mock import Mock, MagicMock
 
 from .models import Human
 
@@ -8,12 +6,7 @@ from .models import Human
 class HumanTestCase(TestCase):
     def setUp(self):
         Human.objects.create(name="cslee", description="healthful guy", likes=0)
-        # Human.objects.create(name="AI", description="healthful AI", likes=99)
 
-    def test_queryset_exists(self):
-        qs = Human.objects.all()
-        self.assertTrue(qs.exists)
-
-    def test_queryset_correct(self):
-        qs = Human.objects.get(name="cslee")
-        self.assertEqual(qs.name, "cslee")
+    def test_human_name(self):
+        human = Human.objects.get(pk=1)
+        self.assertEqual(human.name, 'cslee')
